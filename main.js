a401a6ea302d4197f456be1b8cb1fd038780104f
lilian manaa <lelian.ibraheem@gmail.com>
2022-10-23 03:26:06 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -4,6 +4,41 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const like = document.querySelectorAll(".like-glyph")
+const hiddenClass = document.getElementById("modal")
+
+like.forEach(i => {
+  i.addEventListener("click", handleLike)
+})
+
+function handleLike(e){
+  const heart = e.target
+  console.log(heart)
+  mimicServerCall()
+    .then((message)=>{
+      console.log(message)
+      hiddenClass.innerText = message
+      hiddenClass.classList.remove("hidden")
+      setTimeout(()=>{hiddenClass.classList.add("hidden")}, 3000)
+
+      if(heart.innerText === EMPTY_HEART){
+        heart.innerText = FULL_HEART
+        heart.classList.add("activated-heart")
+      }else if(heart.innerText === FULL_HEART){
+        heart.innerText = EMPTY_HEART
+        heart.classList.remove("activated-heart")
+      }
+    })
+    .catch((error)=>{
+       console.error(error);
+       console.log("error") 
+    })
+    // e.preventdefault()
+    // console.log(e.target.value)
+    // console.log("hi")
+}
+
+
 
 
 
@@ -15,7 +50,7 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
+      if (isRandomFailure) { 
         reject("Random server error. Try again.");
       } else {
         resolve("Pretend remote server notified of action!");
